03af747f6dcc9c4109831ac10228cb841bd2814a
Adnan Islam <iadnan7090@gmail.com>
2026-02-16 18:48:30 +1100

    new cell struct

--- Wordle/src/Interface/WordleInterface.cs
+++ Wordle/src/Interface/WordleInterface.cs
@@ -1,57 +1,24 @@
 ﻿namespace Wordle.Interface;
 
-
 using System.IO;
 using System;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Processing;
 
-
+public struct Cell(int rowIndex, int colIndex, string letter)
+{
+    public int rowIndex = rowIndex;
+    public int colIndex = colIndex;
+    public string letter = letter;
+}
 public class WordleInterface
 {  
-    
-    //public void SettingTheScreenToAnImage(string imageFilepath)
-    //{
-    //    var width = Console.WindowWidth;
-    //    var height = Console.WindowHeight;
-    //    Image<Rgba32> img;
-//
-    //    try
-    //    {
-    //        img = Image.Load<Rgba32>(imageFilepath);
-    //    }
-    //    catch
-    //    {
-    //        Console.WriteLine("File is not a valid image file");
-    //        return;
-    //    }
-    //    
-    //    img.Mutate(x => x.Resize(width, height*2));
-//
-    //    for (int y = 0; y < height; y++)
-    //    {
-    //        for (int x = 0; x < width; x++)
-    //        {
-    //            Rgba32 topPixel = img[x, y*2];
-    //            Rgba32 bottomPixel = img[x, y*2+1];
-    //            
-    //            Console.Write($"\x1B[38;2;{topPixel.R};{topPixel.G};{topPixel.B};48;2;{bottomPixel.R};{bottomPixel.G};{bottomPixel.B}m▀\x1B[0m");
-    //        }
-    //    }
-    //}
-
+    private Cell[,] _cells;
     public void Display()
     {
         Console.Clear();
         var height = Console.WindowHeight;
         var width = Console.WindowWidth;
         var horizontalPadding = (width - 40) / 2;
-        var verticalPadding = (height - 49) / 2;
-
-        if (horizontalPadding < 0) horizontalPadding = 0;
-        if (verticalPadding < 0) verticalPadding = 0;
-        
+        var verticalPadding = (height - 24) / 2;
         
         string baseDir = @"C:\Users\atnm\RiderProjects\ConsoleApp1\Wordle";
         string resourcesDir = Path.Combine(baseDir, "Resources");
@@ -89,6 +56,7 @@ public class WordleInterface
     public WordleInterface()
     {
         Console.Clear();
+        this._cells = new Cell[5, 6];
     }
     
     
